4ba4289307f55ea97acdde531c1ac4497cccdbf3
holmesrm88 <holmesrm88@gmail.com>
2021-11-04 19:57:10 -0400

    add bean validation to BeerDto

--- src/main/java/com/example/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/example/msscbeerservice/web/model/BeerDto.java
@@ -3,6 +3,10 @@ package com.example.msscbeerservice.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -12,17 +16,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime modifiedDate;
 
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;

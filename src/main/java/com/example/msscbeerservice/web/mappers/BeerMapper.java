c807c400447deaa8ef4e59039c250b4f821682de
holmesrm88 <holmesrm88@gmail.com>
2021-11-06 17:02:52 -0400

    implement mapstruct and add beermapper and datemapper

---
+++ src/main/java/com/example/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package com.example.msscbeerservice.web.mappers;
+
+import com.example.msscbeerservice.domain.Beer;
+import com.example.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto dto);
+}

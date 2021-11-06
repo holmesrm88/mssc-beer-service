3992c4ca08c9e613628b918a7fe59f68e99160fe
holmesrm88 <holmesrm88@gmail.com>
2021-11-06 19:09:44 -0400

    add mock mvc configuration to test class

--- src/test/java/com/example/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/example/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,26 +1,34 @@
 package com.example.msscbeerservice.web.controller;
 
+import com.example.msscbeerservice.domain.Beer;
+import com.example.msscbeerservice.repositories.BeerRepository;
 import com.example.msscbeerservice.web.model.BeerDto;
 import com.example.msscbeerservice.web.model.BeerStyleEnum;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.example.msscbeerservice.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -29,8 +37,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }

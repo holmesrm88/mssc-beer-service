90b0862863f7b99a35cbfaf013242c7abbdb4dfc
holmesrm88 <holmesrm88@gmail.com>
2021-11-07 19:48:28 -0500

    jacksn property naming stragegies

---
+++ src/test/java/com/example/msscbeerservice/web/model/BeerDtoSnakeTest.java
@@ -0,0 +1,26 @@
+package com.example.msscbeerservice.web.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@JsonTest
+@ActiveProfiles("snake")
+public class BeerDtoSnakeTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto dto = getDto();
+
+        String json = objectMapper.writeValueAsString(dto);
+
+        System.out.println(json);
+    }
+
+}

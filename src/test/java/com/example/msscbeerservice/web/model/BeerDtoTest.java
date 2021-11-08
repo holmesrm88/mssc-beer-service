07efc445ed61b9a72374fef7fb3bfcf969e05a15
holmesrm88 <holmesrm88@gmail.com>
2021-11-08 10:38:16 -0500

    fix test

--- src/test/java/com/example/msscbeerservice/web/model/BeerDtoTest.java
+++ src/test/java/com/example/msscbeerservice/web/model/BeerDtoTest.java
@@ -18,7 +18,7 @@ class BeerDtoTest extends BaseTest{
 
     @Test
     void testDeserialized() throws JsonProcessingException {
-        String json = "{\"version\":null,\"createdDate\":\"2021-11-08T10:20:06-0500\",\"lastModifiedDate\":\"2021-11-08T10:20:06.465118-05:00\",\"beerName\":\"Matts Beer\",\"beerStyle\":\"ALE\",\"upc\":293857390,\"price\":\"8.99\",\"quantityOnHand\":null,\"myLocalDate\":\"20211108\",\"beerId\":\"7b0ee154-d0a2-4d98-bdab-71c55c897056\"}\n";
+        String json = "{\"version\":null,\"createdDate\":\"2021-11-08T10:37:21-0500\",\"lastModifiedDate\":\"2021-11-08T10:37:21-0500\",\"beerName\":\"Matts Beer\",\"beerStyle\":\"ALE\",\"upc\":293857390,\"price\":\"8.99\",\"quantityOnHand\":null,\"myLocalDate\":\"20211108\",\"beerId\":\"35698dfc-dff8-4956-9a69-ad7050bdb67c\"}\n";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 

324a794a3eb82fca78b6fde1d2844bbb790c4958
holmesrm88 <holmesrm88@gmail.com>
2021-11-10 08:16:36 -0500

    add data.sql with data + remove loadBeerObjects from BeerLoader + add to application.properties

--- src/main/java/com/example/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/example/msscbeerservice/bootstrap/BeerLoader.java
@@ -6,10 +6,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-@Component
+//@Component
 public class BeerLoader implements CommandLineRunner {
-
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
     public static final String BEER_1_UPC = "0373263858737";
     public static final String BEER_2_UPC = "0003746281247";
     public static final String BEER_3_UPC = "0016734700245";
@@ -22,7 +25,7 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        loadBeerObjects();
+        //loadBeerObjects();
     }
 
     private void loadBeerObjects() {
